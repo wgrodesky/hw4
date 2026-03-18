72410b37299821b4da0373735cba7af6fa6adb3e
wgrodesky <will.grodesky@kellogg.northwestern.edu>
2026-03-18 03:30:30 +0000

    Homework updates

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+  belongs_to :place
+  belongs_to :user
+  has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
+  get "/signup", to: "users#new"
   resources "entries"
   resources "places"
   resources "sessions"
